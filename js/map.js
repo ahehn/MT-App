d36deb5dde0f26df938cfc44660199472d388f9a
christopher-weber <christopher-weber@web.de>
2013-08-11 16:02:36 +0200

    deleted several files, added maps.js and changed navigation.html

--- js/map.js
+++ js/map.js
@@ -1,67 +1,18 @@
-var karte;
-var marker;
-var meineLongitude, meineLatitude;
 
-/* Karte */
 
-var positionsAusgabe = function(position){
-	width = document.getElementById('karte').offsetWidth;
-	height = document.getElementById('karte').offsetHeight;
-
-	meineLongitude = position.coords.longitude;
-	meineLatitude = position.coords.latitude;
-	
-	var optionen = {
-		zoom: 13,
-		center: new google.maps.LatLng(meineLatitude, meineLongitude),
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-
-	karte = new google.maps.Map(document.getElementById('karte'), optionen);
-	
-	window.setTimeout(function(){
-		
-		$.getJSON('daten/restaurants.json?='+Date.now(), function(data){
-			
-			$('#uebersicht').find('ul').html('');
-			
-			$.each(data, function(restaurant, daten){
-				$('#uebersicht').find('ul').append('<li class="'+daten.Kategorie+'"><img src="http://maps.google.com/maps/api/staticmap?center='+daten.Position.Latitude+','+daten.Position.Longitude+'&zoom=13&size=50x50&markers=color:blue|size:tiny|'+daten.Position.Latitude+','+daten.Position.Longitude+'&sensor=true"/>'+restaurant+'<span>'+entfernungBerechnen(meineLongitude,meineLatitude,daten.Position.Longitude,daten.Position.Latitude)+'</span></li>');
-				
-				marker = new google.maps.Marker({
-					map: karte,
-					animation: google.maps.Animation.DROP,
-					position: new google.maps.LatLng(daten.Position.Latitude,daten.Position.Longitude)
-				});
-				
+function set_marker(){
+	// Auslesen der JSON-Datei und zeichnen von Google-Maps-Markern
+	$.getJSON('../daten/gebaeude.json', function(json) {
+		$.each(json, function(bau,data) {
+			var latLng = new google.maps.LatLng(data.position.latitude, data.position.longitude); 
+			// Creating a marker and putting it on the map
+			var marker = new google.maps.Marker({
+				position: latLng,
+				map: map,
+				title: data.name,
+				icon: '../bilder/' + bau + '.png'
 			});
-			
-		//	window.scrollTo(0,1);
-			
 		});
-		
-	},1); 
-};
-
-	
-/* Toolbar Funktionen */
-
-$('nav a').on('click',function(){
-	$('section, nav a').removeClass('aktiv');
-	$(this).addClass('aktiv');
-	$($(this).attr('href')).addClass('aktiv');
-	return false;
-});
-
-/* Optionen */
-$('#optionen :checkbox').on('change', function(){
-	console.log('#uebersicht .'+this.className);
-	$('#uebersicht li.'+this.className).toggleClass('versteckt');
-});
-
-/* Wird bei App-Start ausgeführt */
-
-$(function(){
-	navigator.geolocation.getCurrentPosition(positionsAusgabe);	        
-    $('a[href=#karte], #karte').addClass("aktiv");
-});
+	});
+alert('marker buildings success');
+}
