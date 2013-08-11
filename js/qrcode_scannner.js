2f79106a1c92af0513d86dc44955be5946745fd2
christopher-weber <christopher-weber@web.de>
2013-08-11 20:36:57 +0200

    added qrcodes.json, changed navigation.html, changed qrcode_scanner.js

--- js/qrcode_scannner.js
+++ js/qrcode_scannner.js
@@ -5,10 +5,18 @@ $(document).ready(function(){
 			var scanner = cordova.require("cordova/plugin/BarcodeScanner");
 			scanner.scan(
 			  function (result) {
-				  alert("We got a barcode\n" +
+				  /*alert("We got a barcode\n" +
 						"Result: " + result.text + "\n" +
 						"Format: " + result.format + "\n" +
-						"Cancelled: " + result.cancelled);
+						"Cancelled: " + result.cancelled);*/
+				// Check if Result is a QR-Code
+				if(result.format == 'QR_CODE'){
+					alert('das ist ein QR-Code')
+				}
+				else{
+				no_qr;
+				}		
+						
 			  }, 
 			  function (error) {
 				  alert("Scanning failed: " + error);
@@ -16,3 +24,11 @@ $(document).ready(function(){
 			);
 		});
 	});
+	
+function scan_invalid(){
+	alert('Sorry, aber das ist kein gültiger QR-Code zum Lokalisieren der Position!');
+}
+
+function no_qr(){
+	alert('Sorry, aber das ist kein QR-Code!');
+}
