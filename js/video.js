5650a891a1f3148462083a9b44dd0f2f82169afc
ahehn <adobe@ahehn.de>
2013-08-12 19:23:39 +0200

    Bugfix: Videos may now be viewed with Android 2.3

--- js/video.js
+++ js/video.js
@@ -1,16 +1,17 @@
-var video;
-
+var video = [];
 function setValue() {
-	video = document.getElementById('video');
-	video.addEventListener('click',playControl,false);
+	<!-- for (var i = 0; i<3; i++) { -->
+	video.push(document.getElementById("video1"));
+	video[0].addEventListener('click',function(){video[0].play();},false);
+	video.push(document.getElementById("video2"));
+	video[1].addEventListener('click',function(){video[1].play();},false);
+	video.push(document.getElementById("video3"));
+	video[2].addEventListener('click',function(){video[2].play();},false);
+	video.push(document.getElementById("video4"));
+	video[3].addEventListener('click',function(){video[3].play();},false);
+	video.push(document.getElementById("video5"));
+	video[4].addEventListener('click',function(){video[4].play();},false);
+	video.push(document.getElementById("video6"));
+	video[5].addEventListener('click',function(){video[5].play();},false);
 }
-
-function playControl() {
-	if (video.paused == false) {
-		video.pause();
-	} else {
-		video.play();
-	}
-}
-	
 window.onload = setValue;
