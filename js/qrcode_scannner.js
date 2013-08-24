048e245f05bffcae38f25ff6d4b74f9789fb11cb
christopher-weber <christopher-weber@web.de>
2013-08-24 19:47:29 +0200

    Bugfix: Picture in "My Position" Popup too large

--- js/qrcode_scannner.js
+++ js/qrcode_scannner.js
@@ -28,7 +28,7 @@ $(document).ready(function(){
 										f_validcode = true;
 										alert('Du bist hier: ' + data.name + '     Etage: ' + data.etage + '     Gebäude: '+ data.bau);
 										$('#positiontext').html(
-										'<h3>' + data.name + '</h3> <p>Etage: ' + data.etage + '</p> <p>Gebäude: ' + data.bau + '</p> <p><img src="../bilder/navigation/' + code + '.png" alt="Gebäude ' + data.bau + 'F, ' + data.etage + '"></p>');
+										'<h3>' + data.name + '</h3> <p>Etage: ' + data.etage + '</p> <p>Gebäude: ' + data.bau + '</p> <p><img src="../bilder/navigation/' + code + '.png" style="max-width:80%;" alt="Gebäude ' + data.bau + 'F, ' + data.etage + '"></p>');
 										/*var text = document.getElementById('possitiontext');
 										text.innerHTML = 'Du bist hier: ' + data.name + '     Etage: ' + data.etage + '     Gebäude: '+ data.bau;*/
 										$("#position").popup("open", {positionTo: "window"});
