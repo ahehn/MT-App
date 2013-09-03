78b7187333817df75508bc11132238f96f79ea72
christopher-weber <christopher-weber@web.de>
2013-09-03 11:37:34 +0200

    Bugfix: Building plans, navbar icons and navigation.html

--- js/campus_map.js
+++ js/campus_map.js
@@ -26,17 +26,7 @@
 			
 			// onSuccess Geolocation
 			//
-			function onSuccess(position) {
-				/*var element = document.getElementById('geolocation');
-				element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
-									'Longitude: '          + position.coords.longitude             + '<br />' +
-									'Altitude: '           + position.coords.altitude              + '<br />' +
-									'Accuracy: '           + position.coords.accuracy              + '<br />' +
-									'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
-									'Heading: '            + position.coords.heading               + '<br />' +
-									'Speed: '              + position.coords.speed                 + '<br />' +
-									'Timestamp: '          + position.timestamp                    + '<br />';*/
-									
+			function onSuccess(position) {							
 				$('#geolocation').html('Latitude: '           + position.coords.latitude              + '<br />' +
 									'Longitude: '          + position.coords.longitude             + '<br />' +
 									'Altitude: '           + position.coords.altitude              + '<br />' +
